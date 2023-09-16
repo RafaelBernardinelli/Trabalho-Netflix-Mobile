9a696008cb84caad3b732e1a4b86128d96042fe8
iroliveira <iroliveira@viptech.com.br>
2023-09-15 21:46:44 -0300

    feat(app): add icons on tab bar

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import Login from "./src/screens/Login";
-import Omdb from "./src/screens/Omdb/Omdb";
-import Details from "./src/screens/Tmdb/Details";
-import TmdbPage from "./src/screens/Tmdb/TmdbPage";
+import { AntDesign, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import Login from './src/screens/Login';
+import Omdb from './src/screens/Omdb/Omdb';
+import Details from './src/screens/Tmdb/Details';
+import TmdbPage from './src/screens/Tmdb/TmdbPage';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,10 +13,34 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Recomendados" component={TmdbPage} />
+        <Tab.Screen
+          name="Login"
+          component={Login}
+          options={{
+            tabBarIcon: () => (
+              <MaterialIcons name="login" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Recomendados"
+          component={TmdbPage}
+          options={{
+            tabBarIcon: () => (
+              <AntDesign name="check" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Pesquisa"
+          component={Omdb}
+          options={{
+            tabBarIcon: () => (
+              <Ionicons name="md-search" size={24} color="black" />
+            ),
+          }}
+        />
         <Tab.Screen name="Detalhes" component={Details} />
-        <Tab.Screen name="Pesquisa" component={Omdb} />
       </Tab.Navigator>
     </NavigationContainer>
   );
